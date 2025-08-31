68542d6caf7a594b14c3627347cee99cf51d96ad
ActualLearner <abdellahshafi7@gmail.com>
2025-08-31 19:18:50 +0300

    add heartbeat logger

---
+++ crm/cron_jobs/cron.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+
+def log_crm_heartbeat():
+    timestamp = datetime.now().strftime("%d/$m/$Y-%H:%M:%S")
+
+    with open("/tmp/crm_heartbeat_log.txt.", "a") as f:
+        f.write(f"{timestamp}")
